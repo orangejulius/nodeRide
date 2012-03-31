2cd0d22c90463823b98c683c62aaf37e25513b74
Julian Simioni <julian.simioni@gmail.com>
2012-03-31 00:38:14 -0700

    Display list of added rides on index

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ exports.upload = require('./upload').index;
  */
 exports.index = function(req, res){
 	var redis = require('../redis.js').index;
-	redis.hgetall('2', function(err, result) {
-  	if (err) {
-		console.log("Error: "+err);
-	} else {
-		console.log(result);
-		res.render('index', { title: 'NodeRide', data: result})
+	redis.keys('meta-*', function(err, result) {
+		if (err) {
+			console.log("Error: "+err);
+		} else {
+			console.log(result);
+			res.render('index', { title: 'NodeRide', data: result})
 		}
 	});
 };
