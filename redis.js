2cf8ddd865ca0e3f8fcb361719dbb719390e348d
Julian Simioni <julian.simioni@gmail.com>
2012-03-30 22:17:28 -0700

    Fix redistogo code

--- redis.js
+++ redis.js
@@ -1,7 +1,5 @@
 if (process.env.REDISTOGO_URL) {
 	var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
-
-	redis.auth(rtg.auth.split(":")[1]);
 } else {
 	var redis = require('redis').createClient();
 }
