3a635279bbb0c8615fd177a9a82a35cedf2b1cc2
Julian Simioni <julian.simioni@gmail.com>
2012-03-30 18:01:59 -0700

    Rename redis client variable

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,13 @@ exports.upload = require('./upload').index;
 exports.index = function(req, res){
 	if (process.env.REDISTOGO_URL) {
 		var rtg = require('url').parse(process.env.REDISTOGO_URL);
-		var client = require('redis').createClient(rtg.port, rtg.hostname);
+		var redis = require('redis').createClient(rtg.port, rtg.hostname);
 
-		client.auth(rtg.auth.split(":")[1]);
+		redis.auth(rtg.auth.split(":")[1]);
 	} else {
-		var redis = require('redis');
-		var client = redis.createClient();
+		var redis = require('redis').createClient();
 	}
-	client.hgetall('2', function(err, result) {
+	redis.hgetall('2', function(err, result) {
   	if (err) {
 		console.log("Error: "+err);
 	} else {
