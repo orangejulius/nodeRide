a31eb893e37af6069b6f0b2255f69e1f0f85dd49
Julian Simioni <julian.simioni@gmail.com>
2012-04-02 22:07:14 -0700

    Create a plot based on ride GPS data
    Also use it to center the map

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -1,10 +1,46 @@
 var hello = 'hello';
 console.log(data);
+console.log(typeof data);
+data = JSON.parse(data);
+console.log(typeof data);
+
+function isValidCoord(value) {
+	if (value == 0)
+		return false;
+	if (value == null)
+		return false;
+	return true;
+}
 function initialize() {
+
+	var center = null;
+
+	var pathCoordinates = [];
+	data.laps.forEach(function(lap) {
+		lap.tracks.forEach(function(track) {
+			track.forEach(function(trackpoint) {
+				var newLatLng = new google.maps.LatLng(trackpoint.lat, trackpoint.lon);
+				if (!center)
+					center = newLatLng;
+				if (isValidCoord(trackpoint.lat) && isValidCoord(trackpoint.lon))
+					pathCoordinates.push(newLatLng);
+			});
+		});
+	});
+
 	var myOptions = {
-		center: new google.maps.LatLng(-34.397, 150.644),
+		center: center,
 		zoom: 8,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+	var flightPath = new google.maps.Polyline({
+		path: pathCoordinates,
+		strokeColor: "#FF0000",
+		strokeOpacity: 1.0,
+		strokeWeight: 2
+	});
+
+	flightPath.setMap(map);
 }
