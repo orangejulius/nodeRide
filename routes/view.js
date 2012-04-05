3a80a2bbc68136d72d4aba442b1cb7a30040e306
Julian Simioni <julian.simioni@gmail.com>
2012-04-04 17:38:06 -0700

    Don't return ride JSON data in /view response body

--- routes/view.js
+++ routes/view.js
@@ -1,6 +1,3 @@
 module.exports = function(req, res) {
-	var redis = require('../core/redis.js');
-	redis.get(req.params.id, function(err, result) {
-		res.render('view', { id: req.params.id, title: "Viewing ride "+req.params.id, data: result});
-	});
+	res.render('view', { id: req.params.id, title: "Viewing ride "+req.params.id});
 };
