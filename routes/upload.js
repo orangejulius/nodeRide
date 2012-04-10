ea70bbe08e1e790081eafd80d3dd4dbc44313c73
Julian Simioni <julian.simioni@gmail.com>
2012-04-10 15:33:14 -0700

    Remove stale commented out code

--- routes/upload.js
+++ routes/upload.js
@@ -3,13 +3,6 @@ var logule = require('logule');
 
 module.exports = function(req, res){
 	var format = require('util').format;
-	//req.files.complete(function(err, fields, files) {
-	//	if (err) {
-	//		next(err);
-	//	} else {
-	//		console.log('will read from '+files.upload.path);
-	//	}
-	//});
 	res.send(format('\nuploaded %s (%d Kb) to %s as %s'
 	  , req.files.upload.name
 	  , req.files.upload.size / 1024 | 0
