867316c59fc4684e13adba5e134171af722cfaad
Julian Simioni <julian.simioni@gmail.com>
2012-04-12 22:13:06 -0700

    Only send a response for /upload when successful
    Previously the response was sent immediately and then the ride processed
    afterwords

--- routes/upload.js
+++ routes/upload.js
@@ -3,12 +3,12 @@ var logule = require('logule');
 
 module.exports = function(req, res){
 	var format = require('util').format;
-	res.send(format('\nuploaded %s (%d Kb) to %s as %s'
-	  , req.files.upload.name
-	  , req.files.upload.size / 1024 | 0
-	  , req.files.upload.path
-	  , req.body.title));
 	readTCX(req.files.upload.path, function(result) {
+		res.send(format('\nuploaded %s (%d Kb) to %s as %s'
+		  , req.files.upload.name
+		  , req.files.upload.size / 1024 | 0
+		  , req.files.upload.path
+		  , req.body.title));
 		logule.info("result for upload of file "+req.files.upload.name + ": "+result);
 	});
 };
