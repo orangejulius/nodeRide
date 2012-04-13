bb2c2ef0703f83b0c53cae419dcb35c1b9ab2e22
Julian Simioni <julian.simioni@gmail.com>
2012-04-12 19:27:43 -0700

    Update DOM when checking for new rides only if ride list changed

--- public/javascripts/noderide.js
+++ public/javascripts/noderide.js
@@ -1,18 +1,46 @@
-function updateRideList() {
+var rideList = null;
+
+//compare two arrays
+//return true if they are the same (same # of elements, all elements equal)
+//false if not
+function compare(array1, array2) {
+	if (array1.length != array2.length) {
+		return false;
+	}
+	for (var i = 0; i < array1.length; i++) {
+		if (array1[i] != array2[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+//check if new rides are available, and if they are, update the DOM with updateRideList
+function checkRideList() {
 	$.getJSON('/get/rideList', function(data) {
-		$('#rideList').empty();
-		$("<li class=\"nav-header\">existing rides</li>").appendTo('#rideList');
-		data.sort();
-		$.each(data, function(i,item) {
-			$("<li><a href=\"/view/#"+item+"\">"+item+"</a></li>").appendTo('#rideList');
-		});
+		if (rideList && compare(data, rideList) == false) {
+			updateRideList(data);
+		} else {
+			updateRideList(data);
+		}
+		rideList = data;
+	});
+}
+
+//given a json object with a list of rides, update the DOM with new elements
+function updateRideList(data) {
+	$('#rideList').empty();
+	$("<li class=\"nav-header\">existing rides</li>").appendTo('#rideList');
+	data.sort();
+	$.each(data, function(i,item) {
+		$("<li><a href=\"/view/#"+item+"\">"+item+"</a></li>").appendTo('#rideList');
 	});
 }
 
 function initializeNodeRide() {
-	updateRideList();
+	checkRideList();
 
-	setInterval("updateRideList()", 2000);
+	setInterval("checkRideList()", 2000);
 
 	$(':button').click(function() {
 		var formData = new FormData($('form')[0]);
