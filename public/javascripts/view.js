351dbe68b0ee68969434dc84c9cd34824ad20470
Julian Simioni <julian.simioni@gmail.com>
2012-04-15 19:46:35 -0700

    Initial plot graphing elevation

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -62,16 +62,18 @@ function initializeMap() {
 }
 
 function updatePlot(rideData) {
-	var d1 = [];
-	for (var i = 0; i < 14; i += 0.5)
-		d1.push([i, Math.sin(i)]);
+	var elevation = [];
+	var i = 0;
+	rideData.laps.forEach(function(lap) {
+		lap.tracks.forEach(function(track) {
+			track.forEach(function(trackpoint) {
+				elevation.push([i, trackpoint.alt]);
+				i++;
+			});
+		});
+	});
 
-	var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
-
-	//a null signifies separate line segments
-	var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
-
-	$.plot($("#plot"), [ d1, d2, d3 ]);
+	$.plot($("#plot"), [ elevation ]);
 }
 
 $(initializeMap);
