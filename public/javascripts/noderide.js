45258d36621ce9585b2202d261ba15e082bcb03a
Julian Simioni <julian.simioni@gmail.com>
2013-04-08 22:18:22 -0700

    Convert RideList to object with prototype

--- public/javascripts/noderide.js
+++ public/javascripts/noderide.js
@@ -1,62 +1,73 @@
-var rideList = null;
+var NodeRide = {
+	RideList: function() {
+		this.initialize();
+	},
+};
 
-//compare two arrays
-//return true if they are the same (same # of elements, all elements equal)
-//false if not
-function compare(array1, array2) {
-	if (array1.length != array2.length) {
-		return false;
-	}
-	for (var i = 0; i < array1.length; i++) {
-		if (array1[i] != array2[i]) {
+NodeRide.RideList.prototype = {
+	rideList: null,
+	//compare two arrays
+	//return true if they are the same (same # of elements, all elements equal)
+	//false if not
+	compare: function(array1, array2) {
+		if (array1.length != array2.length) {
 			return false;
 		}
-	}
-	return true;
-}
-
-//check if new rides are available, and if they are, update the DOM with updateRideList
-function checkRideList() {
-	$.getJSON('/get/rideList', function(data) {
-		if (rideList && compare(data, rideList) == false) {
-			updateRideList(data);
-		} else {
-			updateRideList(data);
+		for (var i = 0; i < array1.length; i++) {
+			if (array1[i] != array2[i]) {
+				return false;
+			}
 		}
-		rideList = data;
-	});
-}
+		return true;
+	},
 
-//given a json object with a list of rides, update the DOM with new elements
-function updateRideList(data) {
-	$('#rideList').empty();
-	$("<li class=\"nav-header\">existing rides</li>").appendTo('#rideList');
-	data.sort();
-	$.each(data, function(i,item) {
-		$("<li><a href=\"/view/#"+item+"\">"+item+"</a></li>").appendTo('#rideList');
-	});
-}
-
-function initializeNodeRide() {
-	checkRideList();
-
-	$('#uploadLink').click(function() {
-		$('input[type=file]').click();
-	});
-
-	$('input[type=file]').change(function() {
-		var formData = new FormData($('form')[0]);
-		$.ajax({
-			url: '/upload',
-			type: 'POST',
-			error: function(err) { console.log('error: '); console.log(err);},
-			success: function() {checkRideList();},
-			data: formData,
-			cache: false,
-			contentType: false,
-			processData: false
+	//check if new rides are available, and if they are, update the DOM with updateRideList
+	checkRideList: function() {
+		var that = this;
+		$.getJSON('/get/rideList', function(data) {
+			if (that.rideList && that.compare(data, that.rideList) == false) {
+				that.updateRideList(data);
+			} else {
+				that.updateRideList(data);
+			}
+			that.rideList = data;
 		});
-	});
+	},
+
+	//given a json object with a list of rides, update the DOM with new elements
+	updateRideList: function(data) {
+		$('#rideList').empty();
+		$("<li class=\"nav-header\">existing rides</li>").appendTo('#rideList');
+		data.sort();
+		$.each(data, function(i,item) {
+			$("<li><a href=\"/view/#"+item+"\">"+item+"</a></li>").appendTo('#rideList');
+		});
+	},
+
+	initialize: function() {
+		this.checkRideList();
+
+		$('#uploadLink').click(function() {
+			$('input[type=file]').click();
+		});
+
+		var that = this;
+		$('input[type=file]').change(function() {
+			var formData = new FormData($('form')[0]);
+			$.ajax({
+				url: '/upload',
+				type: 'POST',
+				error: function(err) { console.log('error: '); console.log(err);},
+				success: function() {that.checkRideList();},
+				data: formData,
+				cache: false,
+				contentType: false,
+				processData: false
+			});
+		});
+	}
 }
 
-$(initializeNodeRide);
+$(document).ready(function() {
+	NodeRide.ridelist = new NodeRide.RideList();
+});
