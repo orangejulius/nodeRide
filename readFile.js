52ddd3c5a2867261d1b2c687f336f064fb945612
Julian Simioni <julian.simioni@gmail.com>
2013-04-12 23:44:43 -0700

    Fix jslint errors

--- readFile.js
+++ readFile.js
@@ -2,7 +2,7 @@ process.argv.slice(2).forEach(function(filename, index) {
 	var readTCX = require('./core/readTCX.js').index;
 	console.log("arg "+filename);
 	readTCX(filename, function(result) {
-		if (result == true) {
+		if (result === true) {
 			console.log("read file successfully");
 			process.exit(0);
 		}
